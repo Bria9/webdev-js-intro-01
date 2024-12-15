3c4d4d34c35f9f5a60df099cdb50903463917ddc
Bri A <alexiellis22@gmail.com>
2024-12-14 20:44:23 -0500

    Complete index.js experiment Sec./comment out code

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+
+
+/* 
+// declaration of constant variables for a cat cafe
+const cafeName = "WhiskerWonderland";
+const state = "Colorado";
+const city = "Aspen";
+
+//attempt to reassign constant variable
+// city = "louisville";
+//TypeError: Assignment to constant variable.
+
+// declaration of let variables for resident cat info
+
+let adoptionStatus = false;
+let healthCondition = "fair" 
+let age = 2;
+console.log(adoptionStatus);
+console.log(healthCondition);
+console.log(age);
+ */
